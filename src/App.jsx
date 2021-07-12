8888c303556019edab2bfe6c5e04f30ed6f5896d
Vijay Golani <vijay_g@ch.iitr.ac.in>
2021-07-12 23:52:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Card(props)
+{
+    return( <>
+
+     <div className="cards">
+        <div className="card">
+        <img src={props.imgsrc} alt="mypic" className="card__img" />
+            <div className="card__info">
+            <br />
+                <span className="card__category">{props.title}</span>
+               
+                <br /> <h3 className="card__title">{props.sname} </h3>
+                <a href={props.link} target="_blank">
+
+                     <button>Watch Now</button>
+                </a>
+            </div>
+        </div>
+     
+     </div>
+     
+     
+     </>  
+    )  
+}
+
+export default Card;
---
+++ src/Data.jsx
@@ -0,0 +1,37 @@
+const Data=[
+    {
+        id: 1,
+    imgsrc:"https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABWKGy4K_1_dvtIW7celhWo6bXLMybAQPY7sAzW2aoc6gUizViVh4WXlTfUy-wJF09wIxphPT0fFx-LwBRmcJ9dEZ3HiR.jpg?r=077" ,
+    title:"A netflix original series",
+
+     sname:'Dark',
+     link:"https://www.netflix.com/in/title/80100172",
+    },
+    {
+        id: 2,
+    imgsrc:"https://www.iwmbuzz.com/wp-content/uploads/2020/04/5-reasons-why-sacred-games-is-the-most-binge-worthy-web-series-across-ott-platforms-in-quarantine-920x518.jpg",
+    title:"A netflix original series",
+    sname:"Sacred Games",
+    link:"https://www.netflix.com/in/title/80115328",
+    },
+    {  id: 3,
+       imgsrc:"https://images.financialexpress.com/2020/02/Netflix-Stranger-Things-season-4.jpg",
+       title:"A netflix original series",
+    sname:"Stranger Things",
+    link:"https://www.netflix.com/in/title/80057281",
+    },
+    { id: 4,
+        imgsrc:"https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2020/04/19/Pictures/_ae72fd94-824c-11ea-bf35-36f2d834daf8.jpg",
+        title:"A netflix original series",
+        sname:"Money Heist",
+        link:"https://www.netflix.com/in/title/80192098",
+    },
+
+    {   id: 5,
+        imgsrc:"https://assets.gqindia.com/photos/5f5f470e5abcb4976c9f7576/16:9/w_2560%2Cc_limit/A%252520still%252520from%252520Peaky%252520Blinders.jpg",
+        title:"A netflix original series",
+        sname:"Peaky Blinders",
+        link:"https://www.netflix.com/in/title/80002479",
+    }
+];
+export default Data;
